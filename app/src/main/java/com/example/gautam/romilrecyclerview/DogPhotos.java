9039e377b9d3b13c8a119da5c44a9c0614fde189
Gautam <gautamhans1@gmail.com>
2017-04-02 13:38:57 -0700

    Added DogClass to hold Dog PhotoID

---
+++ app/src/main/java/com/example/gautam/romilrecyclerview/DogPhotos.java
@@ -0,0 +1,13 @@
+package com.example.gautam.romilrecyclerview;
+
+/**
+ * Created by Gautam on 02-Apr-17.
+ */
+
+public class DogPhotos {
+    int photoId;
+
+    DogPhotos(int photoId){
+        this.photoId = photoId;
+    }
+}
